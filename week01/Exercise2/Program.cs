3635ea0a32d299eaaa5bb72461fa18289e5b69a6
Ebube juliet <uebubejuliet>
2026-05-21 23:17:25 +0100

    complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int Number = int.Parse(userInput);
+
+        string letter = "";
+
+        if (Number >= 90)
+        {
+            letter = "A";
+            
+        }
+        else if (Number >= 80)
+        {
+            letter = "B";
+            
+        }
+        else if (Number >= 70)
+        {
+            letter = "C";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your score is {letter}");
+
+        if (Number >= 70)
+        {
+            Console.Write("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Work hard next time");
+        }
+
     }
 }
