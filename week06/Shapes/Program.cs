109872cc432f1eebb0b04415cc64bb2600c8654b
Ebube juliet <uebubejuliet>
2026-06-11 00:40:18 +0100

    complete

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square sh1 = new Square("blue", 4);
+        shapes.Add(sh1);
+        Rectange sh2 = new Rectange("red", 4, 5);
+        shapes.Add(sh2);
+        Circle sh3 = new Circle(3, "green");
+        shapes.Add(sh3);
+
+        foreach (Shape sh in shapes)
+        {
+            string color = sh.GetColor();
+            double area = sh.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
     }
 }
