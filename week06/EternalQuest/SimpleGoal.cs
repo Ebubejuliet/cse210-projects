327bae6e5e4a07cff55ce7eda8fb0c05e0471bb8
Ebube juliet <uebubejuliet>
2026-06-15 08:57:55 +0100

    complete

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,22 @@
+using System;
+public class SimpleGoal : Goal
+{
+    private bool _IsComplete;
+    public SimpleGoal(string name, string description, string points) : base (name, description, points)
+    {
+        
+    }
+    public override void RecordEvent()
+    {
+        _IsComplete = true;
+        
+    }
+    public override bool IsComplete()
+    {
+        return _IsComplete;
+    }
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal: {_shortName}, {_description}, {_points}";
+    }
+}
