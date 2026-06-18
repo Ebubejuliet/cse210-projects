2649c7acbb18682b10d3ad5e5b28bf8c43054075
Ebube juliet <uebubejuliet>
2026-06-18 10:54:15 +0100

    Made correction

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -22,7 +22,7 @@ public abstract class Activity
     public abstract double GetPace();
     public virtual string GetSummary()
     {
-        return $"{_date} {GetType().Name} ({_lengthMinutes} min)- Distance {GetDistance():F2} km, Speed {GetSpeed():F2} kph, Pace {GetPace():F2} min/km";
+        return $"{_date} {GetType().Name} ({_lengthMinutes} min)- Distance: {GetDistance():F2} km, Speed: {GetSpeed():F2} kph, Pace: {GetPace():F2} min/km";
 
         
     }
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         List<Activity> _activities = new List<Activity>();
-        _activities.Add(new RunningActivity("Jun 18", 3, 5));
-        _activities.Add(new SwimmingActivity("Jun 18", 45, 20));
-        _activities.Add(new CyclingActivity("Jun 18", 40, 40));
+        _activities.Add(new RunningActivity("18 Jun 2026", 3, 5));
+        _activities.Add(new SwimmingActivity("18 Jun 2026", 45, 20));
+        _activities.Add(new CyclingActivity("18 Jun 2026", 40, 40));
         foreach (Activity activity in _activities)
         {
             Console.WriteLine(activity.GetSummary());
